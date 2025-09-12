2899e2ad5ff985da2babedd394794d6ec53efd08
Catrin Tornqvist <catrin.tq@gmail.com>
2025-09-12 11:59:03 +0200

    add af design pattern and add css to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,43 @@
-import { NavLink } from "react-router-dom";
 import { useFavorites } from "../contexts/FavoritesContext";
+import {
+  DigiHeader,
+  DigiHeaderNavigation,
+  DigiHeaderNavigationItem,
+} from "@designsystem-se/af-react";
+import { useLocation } from "react-router-dom";
 
 export const Header = () => {
   const { favorites } = useFavorites();
+  const location = useLocation();
+  const currentPath = location.pathname;
 
   return (
-    <nav>
-      <NavLink to="/"><p>Till Jobb jobb jobb</p></NavLink>{" "}
-      <NavLink to="/favorites">FAV ({favorites.length})</NavLink>
-    </nav>
+    <DigiHeader
+      afSystemName="Platsbanken för utvecklare"
+      afHideSystemName={true}
+      afMenuButtonText="Meny"
+    >
+      <a
+        slot="header-logo"
+        aria-label="Platsbanken för utvecklare"
+        href="/"
+      ></a>
+      <div slot="header-navigation">
+        <DigiHeaderNavigation
+          afCloseButtonText="Stäng"
+          afCloseButtonAriaLabel="Stäng meny"
+          afNavAriaLabel="Huvudmeny"
+        >
+          <DigiHeaderNavigationItem afCurrentPage={currentPath === "/"}>
+            <a href="/">Jobbannonser</a>
+          </DigiHeaderNavigationItem>
+          <DigiHeaderNavigationItem
+            afCurrentPage={currentPath === "/favorites"}
+          >
+            <a href="/favorites">Mina favoriter ({favorites.length})</a>
+          </DigiHeaderNavigationItem>
+        </DigiHeaderNavigation>
+      </div>
+    </DigiHeader>
   );
 };
